be8a474a2e8327e512f31aeb1290e1f6699fa34b
Christina-Repou <chr.repou@gmail.com>
2025-08-31 23:28:00 +0300

    Implementing a simple jpeg to png converter and adding a folder with images for testing

---
+++ main.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import sys
+from pathlib import Path
+
+def jpeg_to_png():
+    try:
+        source_folder = sys.argv[1]
+        dest_folder = sys.argv[2]
+        try:
+            source_path = Path(source_folder)
+            for imageFile in source_path.iterdir():
+                if imageFile.is_dir():
+                    continue
+                img = Image.open(imageFile)
+                try:
+                    dest_path = Path(dest_folder)
+                    dest_path.mkdir(parents=True, exist_ok=True)
+                    img_file_name = imageFile.name.replace('jpeg', 'png')
+                    img.save(dest_path / img_file_name)
+                except FileExistsError as err:
+                    print(err)
+                    break
+        except FileNotFoundError as err:
+            print(err)
+    except IndexError:
+        print('Usage: python3 JpegToPngConverter.py source_folder dest_folder')
+    except Exception as err:
+        print(err)
+
+if __name__ == '__main__':
+    jpeg_to_png()
